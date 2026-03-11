f8de6b60e9a143ec28ca0b4b0719f3513dfc4c0a
Julen Corera Herrera <julen.corera@opendeusto.es>
2026-03-11 18:08:57 +0100

    Modelo de prediccion final.

--- deliverable2.R
+++ deliverable2.R
@@ -38,131 +38,121 @@ plot(mod1)
 # Summary 
 summary(mod1)
 
-
-#There are some variables with high p-value, we discard them
-plot(mod1,1)
-#Kind of a linear trend
-
-#####Backward elimination#####
-#We chose a significance level of alpha= 0.15
-#Firstly, we will discard the high p-value variables
-mod11 <- lm((Lower_Unemployment_Rate) ~ Inf_Secundaria_25_34 +
-              Segunda_Etapa_25_34 + Superior_25_34 + Age12_Suitability + Age15_Suitability +
-              Middle_Unemployment_Rate + Upper_Unemployment_Rate + Lower_Emp_Rate_25_64 + 
-              Lower_Emp_Rate_25_34 + Middle_Emp_Rate_25_64 + Upper_Emp_Rate_25_64 + 
-              Lower_Activity_Rate_25_64 + Lower_Activity_Rate_25_34 + Middle_Activity_Rate_25_64 +
-              Upper_Activity_Rate_25_64 + Sex + Year, data = mixedDf)
-summary(mod11)
-plot(mod11,1)
-
-
-#Removing
-#summary(mod11)
-#Remove year: p value= 0.397919
-mod12 <- lm((Lower_Unemployment_Rate) ~ Inf_Secundaria_25_34 +
-              Segunda_Etapa_25_34 + Superior_25_34 + Age12_Suitability + Age15_Suitability +
-              Middle_Unemployment_Rate + Upper_Unemployment_Rate + Lower_Emp_Rate_25_64 + 
-              Lower_Emp_Rate_25_34 + Middle_Emp_Rate_25_64 + Upper_Emp_Rate_25_64 + 
-              Lower_Activity_Rate_25_64 + Lower_Activity_Rate_25_34 + Middle_Activity_Rate_25_64 +
-              Upper_Activity_Rate_25_64 + Sex, data = mixedDf)
-summary(mod12)
-
-#Now all the variables have small p-values
-#But we still have so many variables, may not be useful
 #Firstly we will detect and eliminate the noise
-
-#Distribution of the residuals
-shapiro.test(residuals(mod12))
-#Small p-value, then the residuals don't follow a normal distribution
-
 ######Checking for outliers######
-plot(mod12,1)
-# stud_resids <- studres(mod12)
-# plot ( mod12$fitted.values , stud_resids,
-#        xlab ='Fitted', ylab ='Studentized Residuals')
+plot(mod1,1)
 
-res_stud <- rstudent(mod12)
+res_stud <- rstudent(mod1)
 outliers <- which(abs(res_stud) > 3)
+print(outliers)
 
 ######Leverage points######
-plot(mod12,5)
-sort(cooks.distance(mod12))
+plot(mod1,5)
+sort(cooks.distance(mod11))
 #We can see that firstly the observations 36, 306 and 373 could be outliers in the 
-#Residuals vs Fitted plot (line 79). 
-#Moreover, looking ate the Residuals vs Leverage plot (line 88), 
-#the value 394 could also be a leverage point
+#Residuals vs Fitted plot (line 43). 
+#Moreover, looking ate the Residuals vs Leverage plot (line 50), 
+#the values 36 abd 394 could also be leverage points
 #Aditionaly, in lines 84 and 85 we computed the outliers with the formula, with this
 #we can see that the observations 303, 370 and 391 are also outliers.
 #We proceed with the elimination
 
-noise_names <- c("36", "303", "306", "370", "373", "391", "394")
+noise_names <- c("36", "268", "271", "368", "370", "371", "373", "391", "394")
 
 #Filter the original dataset finding the four values
 mixedDfSubset <- mixedDf[!(rownames(mixedDf) %in% noise_names), ]
 
 summary(mixedDfSubset)
 
-#We compute again the model
-mod12Clean <- lm((Lower_Unemployment_Rate) ~ Inf_Secundaria_25_34 +
+#Now that the outliers and leverage points have been erased, we will create again our model
+
+mod_clean <- lm((Lower_Unemployment_Rate) ~ Inf_Secundaria_25_64 + Inf_Secundaria_25_34 + 
+                  Inf_Secundaria_55_64 + Segunda_Etapa_25_64 + Segunda_Etapa_25_34 + 
+                  Segunda_Etapa_55_64 + Superior_25_64 + Superior_25_34 + Superior_55_64 +
+                  Age12_Suitability + Age15_Suitability + Middle_Unemployment_Rate + 
+                  Upper_Unemployment_Rate + Lower_Emp_Rate_25_64 + Lower_Emp_Rate_25_34 +
+                  Middle_Emp_Rate_25_64 + Middle_Emp_Rate_25_34 + Upper_Emp_Rate_25_64 + 
+                  Upper_Emp_Rate_25_34 + Lower_Activity_Rate_25_64 + Lower_Activity_Rate_25_34 +
+                  Middle_Activity_Rate_25_64 + Middle_Activity_Rate_25_34 + 
+                  Upper_Activity_Rate_25_64 + Upper_Activity_Rate_25_34 + Sex + Year, data = mixedDfSubset)
+summary(mod_clean)
+
+#####Backward elimination#####
+
+#For the first part of backward elimination, we will use the step() function in order to take
+#the variables that could be relevant.
+#This function does the backward elimination process by iteratively removing predictors
+#in the model in order to optimise the AIC.
+reducedModel <- step(mod_clean, direction = "backward")
+summary(reducedModel)
+# Compare models using AIC and BIC - lowest is better
+AIC(mod_clean, reducedModel)
+BIC(mod_clean, reducedModel)
+#Both the AIC and BIC went down, meaning that the reduced model is better
+
+#We will plot now the Residuals vs Fitted plot
+plot(reducedModel, 1)
+#Kind of a linear trend
+#Now we will check the distribution of the residuals
+shapiro.test(residuals(reduced_model))
+#Slightly below the standard significance level of 0.05 (0.04576), 
+#meaning that the residuals do not follow a normal distribution.
+#However, it is very close to the threshold, so we will assume the
+#normality assumption as "nearly satisfied"
+
+#Now we will go on with the backward elimination, this time manually to reduce the amount of 
+#variables, as we have some that might not be useful. We will determine a significance level
+#of: alpha = 0.05
+
+#Firstly, we will take out the variable Inf_Secundaria_25_34 with p-value of 0.09949
+reducedModel2 <- lm((Lower_Unemployment_Rate) ~ Inf_Secundaria_25_64 + Inf_Secundaria_55_64 + 
                    Segunda_Etapa_25_34 + Superior_25_34 + Age12_Suitability + Age15_Suitability +
                    Middle_Unemployment_Rate + Upper_Unemployment_Rate + Lower_Emp_Rate_25_64 + 
-                   Lower_Emp_Rate_25_34 + Middle_Emp_Rate_25_64 + Upper_Emp_Rate_25_64 + 
+                   Lower_Emp_Rate_25_34 + Middle_Emp_Rate_25_64 + Upper_Emp_Rate_25_64 + Upper_Emp_Rate_25_34 +
                    Lower_Activity_Rate_25_64 + Lower_Activity_Rate_25_34 + Middle_Activity_Rate_25_64 +
-                   Upper_Activity_Rate_25_64 + Sex, data = mixedDfSubset)
-summary(mod12Clean)
-summary(mod12)
-plot(mod12Clean,1)
-plot(mod12Clean,5)
+                   Upper_Activity_Rate_25_64 + Upper_Activity_Rate_25_34 + Year, data = mixedDfSubset)
+summary(reducedModel2)
+plot(reducedModel2,1)
+plot(reducedModel2,5)
 
-#We will reduce the significance level in order to reduce the accuracy of our model 
-#Reduce alpha = 0.05
+# Compare models again by using AIC and BIC
+AIC(reduced_model, reducedModel2)
+BIC(reduced_model, reducedModel2)
+#Although the AIC slightly increased (~ 1 unit more), the BIC also slightly decreased (by ~ 3 units)
+#For our purpose, we will choose the model with less variables
 
-summary(mod12Clean)
+#Delete Superior_25_34 with p-value of 0.14719    
+reducedModel3 <- lm((Lower_Unemployment_Rate) ~ Inf_Secundaria_25_64 + Inf_Secundaria_55_64 + 
+                      Segunda_Etapa_25_34 + Age12_Suitability + Age15_Suitability + Middle_Unemployment_Rate + 
+                      Upper_Unemployment_Rate + Lower_Emp_Rate_25_64 + Lower_Emp_Rate_25_34 + 
+                      Middle_Emp_Rate_25_64 + Upper_Emp_Rate_25_64 + Upper_Emp_Rate_25_34 +
+                      Lower_Activity_Rate_25_64 + Lower_Activity_Rate_25_34 + Middle_Activity_Rate_25_64 +
+                      Upper_Activity_Rate_25_64 + Upper_Activity_Rate_25_34 + Year, data = mixedDfSubset)
 
-#Delete Inf_Secundaria_25_34  with p-value of 0.176158
-mod13 <- lm((Lower_Unemployment_Rate) ~ Segunda_Etapa_25_34 + Superior_25_34 + Age12_Suitability + Age15_Suitability +
-              Middle_Unemployment_Rate + Upper_Unemployment_Rate + Lower_Emp_Rate_25_64 + 
-              Lower_Emp_Rate_25_34 + Middle_Emp_Rate_25_64 + Upper_Emp_Rate_25_64 + 
-              Lower_Activity_Rate_25_64 + Lower_Activity_Rate_25_34 + Middle_Activity_Rate_25_64 +
-              Upper_Activity_Rate_25_64 + Sex, data = mixedDfSubset)
+summary(reducedModel3)
+#AIC and BIC tests
+AIC(reducedModel2, reducedModel3)
+BIC(reducedModel2, reducedModel3)
+#Now, no significant changes have occurred on the AIC, however, the BIC has decreased, meaning that
+#the reducedModel3 could be better, we will use this one.
 
-summary(mod13)
+#Delete Inf_Secundaria_25_64 with p-value of 0.072997
+reducedModel4 <- lm((Lower_Unemployment_Rate) ~ Inf_Secundaria_55_64 + Segunda_Etapa_25_34 + 
+                      Age12_Suitability + Age15_Suitability + Middle_Unemployment_Rate + 
+                      Upper_Unemployment_Rate + Lower_Emp_Rate_25_64 + Lower_Emp_Rate_25_34 + 
+                      Middle_Emp_Rate_25_64 + Upper_Emp_Rate_25_64 + Upper_Emp_Rate_25_34 +
+                      Lower_Activity_Rate_25_64 + Lower_Activity_Rate_25_34 + Middle_Activity_Rate_25_64 +
+                      Upper_Activity_Rate_25_64 + Upper_Activity_Rate_25_34 + Year, data = mixedDfSubset)
 
-#Delete Upper_Activity_Rate_25_64 with p-value of 0.056450
-mod14 <- lm((Lower_Unemployment_Rate) ~ Segunda_Etapa_25_34 + Superior_25_34 + Age12_Suitability + Age15_Suitability +
-              Middle_Unemployment_Rate + Upper_Unemployment_Rate + Lower_Emp_Rate_25_64 + 
-              Lower_Emp_Rate_25_34 + Middle_Emp_Rate_25_64 + Upper_Emp_Rate_25_64 + 
-              Lower_Activity_Rate_25_64 + Lower_Activity_Rate_25_34 + Middle_Activity_Rate_25_64 +
-              Sex, data = mixedDfSubset)
+summary(reducedModel4)
+#AIC and BIC tests
+AIC(reducedModel3, reducedModel4)
+BIC(reducedModel3, reducedModel4)
+#Finally, when deleting the Inf_Secundaria_25_64 predictor, both AIC and BIC increases drastically,
+#meaning that the reduced model isn't the most appropiate one. Then, we will take the reducedModel3
+#as our final model.
+finalModel <- reducedModel3
 
-summary(mod14)
-
-#Delete Superior_25_34 with p-value of 0.086163
-mod15 <- lm((Lower_Unemployment_Rate) ~ Segunda_Etapa_25_34 + Age12_Suitability + Age15_Suitability +
-              Middle_Unemployment_Rate + Upper_Unemployment_Rate + Lower_Emp_Rate_25_64 + 
-              Lower_Emp_Rate_25_34 + Middle_Emp_Rate_25_64 + Upper_Emp_Rate_25_64 + 
-              Lower_Activity_Rate_25_64 + Lower_Activity_Rate_25_34 + Middle_Activity_Rate_25_64 +
-              Sex, data = mixedDfSubset)
-
-summary(mod15)
-
-#Delete Lower_Activity_Rate_25_34 p-value of 0.072197 
-mod16 <- lm((Lower_Unemployment_Rate) ~ Segunda_Etapa_25_34 + Age12_Suitability + Age15_Suitability +
-              Middle_Unemployment_Rate + Upper_Unemployment_Rate + Lower_Emp_Rate_25_64 + 
-              Lower_Emp_Rate_25_34 + Middle_Emp_Rate_25_64 + Upper_Emp_Rate_25_64 + 
-              Lower_Activity_Rate_25_64 + Middle_Activity_Rate_25_64 +
-              Sex, data = mixedDfSubset)
-
-summary(mod16)
-
-#Delete Age12_Suitability with p-value of 0.05507
-mod17 <- lm((Lower_Unemployment_Rate) ~ Segunda_Etapa_25_34 + Age15_Suitability +
-              Middle_Unemployment_Rate + Upper_Unemployment_Rate + Lower_Emp_Rate_25_64 + 
-              Lower_Emp_Rate_25_34 + Middle_Emp_Rate_25_64 + Upper_Emp_Rate_25_64 + 
-              Lower_Activity_Rate_25_64 + Middle_Activity_Rate_25_64 +
-              Sex, data = mixedDfSubset)
-
-summary(mod17)
 
 
 #confidence intervals
@@ -170,11 +160,3 @@ confint(mod12)
 
 # Compare models using R square 
 #c(summary(mod1)$adj.r.squared, summary(mod12)$adj.r.squared)
-
-# Compare models using AIC y BIC - lowest is better
-AIC(mod1, mod12)
-BIC(mod1, mod12)
-
-
-
-
