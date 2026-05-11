dca551b2c8b55781ca2fa515fbe42f15f432b5cd
corerita845 <julen.corera@opendeusto.es>
2026-05-11 18:08:41 +0200

    to pca

--- deliverable4.R
+++ deliverable4.R
@@ -2,7 +2,6 @@ library(FactoMineR)
 library(factoextra)
 library(plotrix)
 library(tidyverse)
-library(dplyr)
 
 data1 <- read.csv("./dataset1.csv")
 data2 <- read.csv("./dataset2.csv")
@@ -81,7 +80,7 @@ res_pca <- PCA(mixedDataFrame,
 
 summary(res_pca)
 # There are 5 variables with a variance higher than 1, however, we will focus on the first two dimensions because they are where 
-# the most big changes of variability occur
+# the most significant changes of variability occur
 
 
 fviz_eig(res_pca, addlabels = TRUE, barfill = "Steelblue", title="Scree Plot: Variance Explained")
@@ -89,6 +88,7 @@ fviz_eig(res_pca, addlabels = TRUE, barfill = "Steelblue", title="Scree Plot: Va
 # components because the cumulative variance explained is of 71.9%. After the third component, the contribution of the next 
 # dimensions decreases significantly. 
 
+######## PCA Model Interpretation ########
 
 # we can verify that our results match those of the pca function
 # eigenvalues
@@ -100,7 +100,7 @@ res_pca$ind$coord
 # variables (correlations)
 res_pca$var$coord
 
-# How the variables are positioned in the two dimensions?
+# How the observations are positioned in the two dimensions?
 fviz_pca_ind(res_pca)
 #  The map reveals a clear structural separation along Dimension 1 (41.3%). What about highlighting the observations by Sex?
 
@@ -123,10 +123,10 @@ fviz_contrib(res_pca, "var", axes = 1, fill = "steelblue", sortcontrib = "desc",
 # employment or activity related variables, meaning that the first component is a dimension of the labor market and unemployment and activity.
 
 fviz_contrib(res_pca, "var", axes = 2, fill = "steelblue", sortcontrib = "desc", top = 15) 
-# This plot shows that the second component is primarly driven by Educational and Suitability variables.
+# This plot shows that the second component is primarily driven by Educational and Suitability variables.
 # This time the most impactful variables are the education ones (higher_education_25_34, higher_education_25_64 and low_secondary_25_34) with
-# more than 10%. Suitability also appears very high. This dimension doesn't measure wether people have a job or not, it measures the level
-# of studies people have and how suitable is the education for the age.
+# more than 10%. Suitability also appears very high. This dimension doesn't measure whether people have a job or not, it measures the level
+# of studies people have and how suitable the education level is for the corresponding age.
 
 # Based on the previous division by sex, it could be said that the shift of the 'Male' group towards the positive side of Dim 1 is driven by 
 # higher employment and activity rates, whereas the 'Female' cluster's position is more influenced by higher unemployment rates in certain regions.
@@ -147,8 +147,35 @@ colSums(res_pca$ind$contrib)
 # of that individual.
 (res_pca$ind$coord/res_pca$ind$dist)^2  #=cos2 for individuals
 # For a variable cos2 is the square of the correlation:
-(res_pca$var$cor)^2 #=cos2 for variables
+((res_pca$var$cor)^2) #=cos2 for variables
 
-# Let's see now the squared cosine graphically
-fviz_cos2(res_pca, choice = "var", axes = 1:2)
+# Let's see now the squared cosine graphically for both dimensions
+fviz_cos2(res_pca, choice = "var", axes = 1)
+# Again, the first dimension explains more the employment variables, explaining very well employment rates between 25-64
+# A gap between mid_activity_25_64 and low_secondary_55_64 cuts the employment variables with the educational ones.
 
+fviz_cos2(res_pca, choice = "var", axes = 2)
+# The second dimension explains more the educational and suitability variables. However, some educational variables (from low_secondary_55_64 
+# to higher_secondary_55_64) are between 0.5 and 0.25. The variable higher_secondary_25_64 isn't explained very well at this dimension, even though 
+# we considered as the educational dimension.
+((res_pca$var$cor)^2)["higher_secondary_25_64",] # The higher_secondary_25_64 is better captured by the third component, and not well represented
+# in the first two components. 
+
+####### Outliers ########
+# In the observations plot, there were some points that were a bit far away from the rest
+# Sort the distances to see the most out of the range observations. Let's see if they correspond to those points that seemed far away
+distances <- sort(res_pca$ind$dist, decreasing = TRUE)
+print(distances)
+head(distances, 5)
+fviz_pca_ind(res_pca)
+# Looking for these variables in the plot, it can be seen that they seemed a bit far away, as they are on the up left side of the plot, slightly
+# far away from the origin. Their far distance value suggest that the observation deviate from the average of their group in terms of education 
+# and labor, existing the possibility of classifying them as outliers, however, as there are many other observations that surpass the distance
+# of 7-8, no clear outcome could be extracted.
+
+# Identify which specific observations (CCAA, Sex, Year) these outliers are
+top_outliers_idx <- order(res_pca$ind$dist, decreasing = TRUE)[1:5]
+mixedDataFrame[top_outliers_idx, c("autonomous_community", "sex", "year")]
+# The most extreme observations correspond exclusively to females in Ceuta y Melilla across different years (2013-2018). 
+# This indicates that this specific group presents a socio-economic profile (likely characterized by extreme unemployment or 
+# low activity rates) that differs drastically from the average, placing them far from the origin of our PCA model.
