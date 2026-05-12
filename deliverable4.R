7bcd459fedcd2ff425e7010037d4a648746779ba
corerita845 <julen.corera@opendeusto.es>
2026-05-12 10:55:27 +0200

    source

--- deliverable4.R
+++ deliverable4.R
@@ -3,69 +3,7 @@ library(factoextra)
 library(tidyverse)
 
 ####### Data Preparation #######
-
-data1 <- read.csv("./dataset1.csv")
-data2 <- read.csv("./dataset2.csv")
-summary(data1)
-summary(data2)
-#names of columns
-names(data1)
-names(data2)
-
-#Mixing both datasets
-mixedDf <- inner_join(data1, data2, by = c("CCAA", "Sex", "Year"))
-
-#Remove NAs values
-mixedDf <- na.omit(mixedDf)
-summary(mixedDf)
-dim(mixedDf)
-
-#We will transform the Sex variable into a binary
-mixedDf$Sex <- factor(mixedDf$Sex, levels = c("Female", "Male"))
-mixedDf$Sex
-head(mixedDf)
-#Rename variables
-mixedDataFrame <- mixedDf %>%
-  rename(
-    autonomous_community = CCAA,
-    sex = Sex,
-    year = Year,
-    
-    low_unemployment_rate = Lower_Unemployment_Rate,
-    mid_unemployment_rate = Middle_Unemployment_Rate,
-    higher_unemployment_rate = Upper_Unemployment_Rate,
-    
-    low_secondary_25_64 = Inf_Secundaria_25_64,
-    low_secondary_25_34 = Inf_Secundaria_25_34,
-    low_secondary_55_64 = Inf_Secundaria_55_64,
-    
-    higher_secondary_25_64 = Segunda_Etapa_25_64,
-    higher_secondary_25_34 = Segunda_Etapa_25_34,
-    higher_secondary_55_64 = Segunda_Etapa_55_64,
-    
-    higher_education_25_64 = Superior_25_64,
-    higher_education_25_34 = Superior_25_34,
-    higher_education_55_64 = Superior_55_64,
-    
-    age12_suitability = Age12_Suitability,
-    age15_suitability = Age15_Suitability,
-    
-    low_employment_25_64 = Lower_Emp_Rate_25_64,
-    low_employment_25_34 = Lower_Emp_Rate_25_34,
-    mid_employment_25_64 = Middle_Emp_Rate_25_64,
-    mid_employment_25_34 = Middle_Emp_Rate_25_34,
-    higher_employment_25_64 = Upper_Emp_Rate_25_64,
-    higher_employment_25_34 = Upper_Emp_Rate_25_34,
-    
-    low_activity_25_64 = Lower_Activity_Rate_25_64,
-    low_activity_25_34 = Lower_Activity_Rate_25_34,
-    mid_activity_25_64 = Middle_Activity_Rate_25_64,
-    mid_activity_25_34 = Middle_Activity_Rate_25_34,
-    higher_activity_25_64 = Upper_Activity_Rate_25_64,
-    higher_activity_25_34 = Upper_Activity_Rate_25_34
-  )
-
-colnames(mixedDataFrame)
+source("deliverable2.R")
 
 ####### PCA Setup #######
 
@@ -126,6 +64,14 @@ fviz_pca_var(res_pca,
 
 # view each variables's contribution to each principle component:
 res_pca$var$contrib
+
+# Biplot: Combining individuals and variables to show the duality clearly
+fviz_pca_biplot(res_pca, 
+                habillage = 2, 
+                addEllipses = TRUE, 
+                col.var = "black", 
+                repel = TRUE,
+                label = "var")
 
 # Let's see it graphically
 fviz_contrib(res_pca, "var", axes = 1, fill = "steelblue", sortcontrib = "desc", top = 15) 
